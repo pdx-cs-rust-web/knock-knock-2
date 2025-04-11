a2bbb89269c737473fa85becdf91eecb9d320b04
Bart Massey <bart.massey@gmail.com>
2025-04-10 18:20:34 -0700

    added css styling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use axum::{self, response, routing};
+use axum_extra::response as extra_response;
 use tokio::net;
 
 struct Joke {
@@ -13,24 +14,30 @@ const THE_JOKE: Joke = Joke {
 
 fn render_joke(joke: &Joke) -> String {
     format!(
-        r#"<p style="font-weight: bold">Knock Knock!</p>
+        r#"<p class="knock">Knock Knock!</p>
 <p>Who's there?</p>
-<p style="font-weight: bold">{}</p>
+<p class="knock">{}</p>
 <p>{} who?</p>
-<p style="font-weight: bold">{}</p>"#,
+<p class="knock">{}</p>"#,
         joke.whos_there,
         joke.whos_there,
         joke.answer,
     )
 }
 
-async fn hello() -> response::Html<String> {
+async fn get_joke() -> response::Html<String> {
     let joke = render_joke(&THE_JOKE);
-    response::Html(format!(r#"<head><title>"Knock Knock!"</title></head><body>{}</body></html>"#, joke))
+    response::Html(format!(r#"<head><title>"Knock Knock!"</title><link rel="stylesheet" href="/knock.css"></head><body>{}</body></html>"#, joke))
+}
+
+async fn get_css() -> extra_response::Css<&'static str> {
+    extra_response::Css(r#".knock {font-weight: bold;}"#)
 }
 
 async fn serve() -> Result<(), Box<dyn std::error::Error>> {
-    let app = axum::Router::new().route("/",  routing::get(hello));
+    let app = axum::Router::new()
+        .route("/",  routing::get(get_joke))
+        .route("/knock.css", routing::get(get_css));
     let listener = net::TcpListener::bind("127.0.0.1:3000").await?;
     axum::serve(listener, app).await?;
     Ok(())
